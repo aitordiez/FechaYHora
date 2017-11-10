459d1268f90b496a4c652de2b9bb1f6cd9590fee
Aitor Diez Fernandez <aitorchic@gmail.com>
2017-11-10 17:23:50 +0100

    Obtenemos la fecha y la hora

--- FechaYHora.java
+++ FechaYHora.java
@@ -62,6 +62,45 @@ public class FechaYHora
             }
         }
     }
+   
+    /**
+     * Permita obtener a través de un método sin parámetros un String que contenga la fecha y la hora
+     */
+    public String getFechaYHora()
+    {
+        {
+         String fechadevolver1;
+         String fechadevolver2;
+         String fechadevolver3;
+         String fechadevolver4;
+         String fechadevolver5;
+         String fechaADevolver;
+         fechadevolver1 = String.valueOf(dias);
+         fechadevolver2 = String.valueOf(meses);
+         fechadevolver3 = String.valueOf(anhos);
+         fechadevolver4 = String.valueOf(horas);
+         fechadevolver5 = String.valueOf(minutos);
+         if (fechadevolver1.length() < 2) {
+             fechadevolver1 = "0" + fechadevolver1;   
+         }
+         if (meses < 10) {
+             fechadevolver2 = "0" + fechadevolver2;    
+         }
+         if (fechadevolver3.length() < 2) {
+             fechadevolver3 = "0" + fechadevolver3;
+         }
+         if (fechadevolver4.length() < 2) {
+             fechadevolver4 = "0" + fechadevolver4;
+         }
+         if (fechadevolver5.length() < 2) {
+             fechadevolver5 = "0" + fechadevolver5;
+         }
+          fechaADevolver = fechadevolver1 + "-" + fechadevolver2 + "-" + fechadevolver3 + " " + fechadevolver4 + ":" + fechadevolver5;
+          return fechaADevolver;
+         
+        }
+    }
+
     
     
 }
